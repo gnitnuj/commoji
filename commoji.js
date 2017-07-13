160288e4b413e8651294ca803ea38c36b23fd52f
Junting <junting.lu@gmail.com>
2017-07-13 12:23:16 -0700

    handle common user errors - user must use -m flag - user cannot provide a blank message

--- commoji.js
+++ commoji.js
@@ -4,10 +4,25 @@ const commander = require('commander');
 const simpleGit = require('simple-git');
 const emojiTranslate = require('moji-translate');
 
+const translate = (msg) => {
+  if (msg === '') {
+    console.error('please provide a commit message');
+    process.exit(1);
+  }
+
+  return emojiTranslate.translate(msg);
+}
+
 // COMMOJI
 commander
   .version('0.0.1')
   .option('-m, --message [message]', 'commits a message or single line')
   .parse(process.argv);
 
-simpleGit().commit(emojiTranslate.translate(commander.message));
+// don't allow submission without -m flag & message
+if (commander.rawArgs.length < 4) {
+  console.error('no command given!');
+  process.exit(1);
+}
+
+simpleGit().commit(translate(commander.message));
